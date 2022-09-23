901cc06135cc748e2350a440384d9bca5da59eb8
yeowool1010 <qyoong3579@gmail.com>
2022-09-24 06:23:29 +0900

    TCP-21 2번 컴포넌트 레이아웃 구현

--- src/data/theme.js
+++ src/data/theme.js
@@ -1,7 +1,9 @@
 const theme = {
     black : '#000000',
     gray : '#DCDCDC',
-    bgColor : '#FAFAFA'
+    bgColor : '#FAFAFA',
+    projectTitleGray: '#575757',
+    projectDateGray: '#949494'
 }
 
 export default theme;

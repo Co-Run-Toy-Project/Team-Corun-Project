3316866478dafa81212ec482b898fe86fb4fe2a8
inside-eun <dameun0808@gmail.com>
2022-09-27 16:06:21 +0900

    TCP-41 1주차 레이아웃 및 애니메이션 구현 완료

--- src/components/our-tech/OurTech.js
+++ src/components/our-tech/OurTech.js
@@ -18,7 +18,8 @@ const MainBox = styled.div`
 
     .my__tech{
         font-family: "NanumSquareBold"; 
-        font-size: 20px;
+        font-weight: bolder;
+        font-size: 30px;
         text-align: center;
         margin-top: 3rem;
         justify-content: center;
@@ -47,38 +48,73 @@ const TabMenu = styled.div`
     margin-top: 3rem;
 
     display: flex;
+    flex-direction: column;
     padding-top: 2rem;
-    justify-content: space-evenly;
 
     transition-duration: 0.3s;
 
     cursor: pointer;
+
+    .imgZone{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+    }
 
     .logo {
         width: 100px;
         height: 100px;
     }
 
-    >img{
+
+
+    .imgZone>img{
+        display: none;
+        filter: grayscale();
+    }
+
+    .imgZone>img:hover{
+        /* background-color: blue; */
+        filter: none;
+    }
+
+    .script{
         display: none;
     }
 
-    >img:hover{
-        background-color: blue;
-    }
-
     &&:hover{
-        background-color: ${(props) => props.theme.black};
+        background-color: lightgray;
+        /* background-color: ${(props) => props.theme.black}; */
     }
     
     &.gray{
-        background-color: ${(props) => props.theme.black};
+        /* background-color: ${(props) => props.theme.black}; */
+        background-color: lightgray;
         height: 18rem;
         transition-duration: 0.3s;
 
-        >img{
+        .imgZone>img{
             display: block;
-            transition-duration: 1s;
+            transition-duration: 0.3s;
+            filter: none;
+        }
+
+        .script{
+            display: flex;
+            padding: 3rem;
+            /* padding-top: rem; */
+
+            .title__script{
+                font-family: NanumSquareBold;
+                font-weight: bolder;
+                font-size: 20px;
+                margin-right: 2rem;
+            }
+
+            .intro__script{
+                font-family: NanumSquareBold;
+                font-size: 16px;
+            }
         }
     }
 
@@ -88,23 +124,29 @@ const TabMenu = styled.div`
         transition-duration: 0.3s;
         margin-top: 2rem;
 
-        >img{
+        .imgZone>img{
             display: none;
-            transition-duration: 1s;
+            transition-duration: 0.3s;  
         }
     }
 
     &.standard{
         background-color: ${(props) => props.theme.bgColor};
-        >img{
+        .imgZone>img{
             display: block;
-            transition-duration: 1s;
+            transition-duration: 0.3s;
         }
     }
 `
 
 const TechZone = () => {
     const [isOpen, setIsOpen] = useState(0);
+
+    // const [color, setColor] = useState("#000000")
+
+    // function hover(){
+    //     setColor
+    // }
 
     const OpenTab = (index) => {
         setIsOpen(index)
@@ -117,15 +159,31 @@ const TechZone = () => {
                 className = {isOpen ===  1 ? "spread gray" :
                                 isOpen ===  0 ?  "standard" 
                                     : "shrink"}>
-                <img className="js logo" src={process.env.PUBLIC_URL + 'image/Unofficial_JavaScript_logo_2.svg'}></img>
-                <img className="css logo" src={process.env.PUBLIC_URL + 'image/CSS3_logo_and_wordmark.svg'}></img>
-                <img className="react logo" src={process.env.PUBLIC_URL + 'image/React-icon.svg'}></img>
+                <div className = "imgZone">
+                    <img className="js logo" src={process.env.PUBLIC_URL + 'image/Unofficial_JavaScript_logo_2.svg'}></img>
+                    <img className="css logo" src={process.env.PUBLIC_URL + 'image/CSS3_logo_and_wordmark.svg'}></img>
+                    <img className="react logo" src={process.env.PUBLIC_URL + 'image/React-icon.svg'}></img>
+                </div>
+                <div className="script">
+                    <div className="title__script">기술 스택</div>
+                    <div className="intro__script">ES6 문법으로 이용하여 기초적인 문법을 숙지하였으며 DOM을 익숙하게 다룰 수 있습니다. 
+                    React라는 프레임워크를 주로 사용하고 함수형 프로그래밍을 주로 사용합니다.  
+                    style component로 필요한 UI를 컴포넌트별로 구현할 수 있습니다. </div>
+                </div>
             </TabMenu>
             <TabMenu onClick = {isOpen === -1 ? () => OpenTab(0) : () => OpenTab(-1)} className = {isOpen ===  -1 ? "spread gray" :
                 isOpen ===  0 ?  "standard" : "shrink"}>
-                <img className="js logo" src={process.env.PUBLIC_URL + 'image/Unofficial_JavaScript_logo_2.svg'}></img>
-                <img className="css logo" src={process.env.PUBLIC_URL + 'image/CSS3_logo_and_wordmark.svg'}></img>
-                <img className="react logo" src={process.env.PUBLIC_URL + 'image/React-icon.svg'}></img>        
+                <div className="imgZone">
+                    <img className="js logo" src={process.env.PUBLIC_URL + 'image/Figma-logo.svg'}></img>
+                    <img className="css logo" src={process.env.PUBLIC_URL + 'image/Git-logo.svg'}></img>
+                    <img className="react logo" src={process.env.PUBLIC_URL + 'image/jira-1.svg'}></img> 
+                </div>
+                <div className="script">
+                    <div className="title__script">협업 스택</div>
+                    <div className="intro__script">git flow방식으로 팀원들과 협업을 진행할 수 있으며, 
+                    figma로 사용자들에게 익숙한 ux및 전체적인 ui에서의 통일성 및 연관성을 논리적으로 생각할 수 있습니다. 
+                    jira를 사용하며 본인의 이슈 및 진행상황을 팀원들과 공유할 수 있습니다.  </div>
+                </div>
             </TabMenu> 
         </MainBox>
     )

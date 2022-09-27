4845fa17db2495c8d3b4403feebf4b00ea2a90ee
dam-eun <103915161+Inside-eun@users.noreply.github.com>
2022-09-27 21:37:21 +0900

    TCP-29 첫 주차 레이아웃, 애니메이션 구현안  (#7)
    * TCP-28 manifest error fix (#1)
    
    * TCP-40 기초적인 레이아웃 및 기능 구현
    
    * TCP-41 1주차 레이아웃 및 애니메이션 구현 완료
    
    Co-authored-by: Oheun Kim <dhdms8696@naver.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import { ThemeProvider } from 'styled-components';
 import theme from "./data/theme";
+import OurTech from './components/our-tech/OurTech';
 
 function App() {
   return (
     <ThemeProvider theme = {theme}>
-      
+      <OurTech></OurTech>
     </ThemeProvider>
   );
 }

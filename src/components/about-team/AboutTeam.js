537c4ef567954a2312d2e378b2e59238e3c6e718
hun0613 <106587166+hun0613@users.noreply.github.com>
2022-10-01 23:26:32 +0900

    TCP-16 AboutTeam 컴포넌트 스크롤 애니메이션 구현완료 (#13)
    * TCP-16 install framer-motion, react-router-dom library
    
    * TCP-16 AboutTeam component Scroll Animation  구현 완료

--- src/components/about-team/AboutTeam.js
+++ src/components/about-team/AboutTeam.js
@@ -1,132 +1,128 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import { Link } from 'react-router-dom';
 // import click from "./icons/click";
 
 
 const Container = styled.div`
     width : 100vw;
     height : 100vh;
+
     background-color : ${(props) => props.theme.bgColor};
-    border : 1px solid red;
- 
+
+    display : flex;
+    flex-direction : column;
+    justify-content : center;
+    align-items : center;
+
+
+    .icon-bulb{
+        font-size : 70px;
+        margin-bottom : 20px;
+    }
+
     > h1 {
-        font-family : "NanumGothic";
-        font-weight : 800;
-        font-size : 200px;
-        top : 10px;
-        left : 3%;
-        position : absolute;
-        /* left : 50px; */
-        color : ${(props) => props.theme.gray};
-        /* z-index : 1; */
+        margin-bottom : 50px;
     }
 
-    > .question {
-        /* padding-top : 100px; */
-        padding-left : 30px;
-        top : 100px;
-        left : 3%;
-        font-size : 40px;
+    button {
+        border-radius : 30px;
+        border : 3px solid ${(props) => props.theme.blue};
+        width : 300px;
+        height : 40px;
+        background-color : white;
 
-        font-family : "NanumGothic";
-        font-weight : 800;
-        /* font-size : 3vw; */
+        font-family : 'NanumGothic';
+        font-size : 15px;
+        font-weight : 700;
+        color : ${(props) => props.theme.blue};
 
-        position : absolute;
 
-        /* z-index : 5; */
-    }
-
-    > .quote-left {
-        font-family : "NanumGothic";
-        font-weight : 800;
-        font-size : 200px;
-
-        top : 28%;
-        left : 20%;
-
-        position : absolute;
+        &:hover {
+        cursor: pointer;
 
     }
-
-    > .quote-right {
-        font-family : "NanumGothic";
-        font-weight : 800;
-        font-size : 200px;
-
-        top : 50%;
-        left : 75%;
-
-        position : absolute;
-
     }
 
-    > .content {
-        /* border : 1px solid red; */
-        display : flex;
-        flex-direction : column;
-        justify-content : center;
-        align-items : center;
-
-
-        font-size : 25px;
-
-        font-family : "NanumGothic";
-        font-weight : 800;
-
-
-        padding-top : 20%;
-    }
-    > .footer {
-
-        padding-top : 20%;
-        /* border : 1px solid red; */
-        display : flex;
-        justify-content : center;
-        
-        > .moreInformation {
     
-            width : 230px;
-            display : flex;
-            justify-content : center; 
-            align-items : center;
-    
-            border-bottom : 1px solid;
-            position : absolute;
 
-            cursor: pointer;
-
-            > .icon-area {
-                > img {
-                    width : 20px;
-                    height : auto;
-                }
-            }
-    
-            > .icon-text {
-                font-family : 'NanumGothic';
-                margin-left : 10px;
-
-                cursor: pointer;
-            }
-        }
-
-    }
 `
 
+const imageAnimate={
+    offscreen:{x:-100, opacity:0},
+    onscreen:{
+    x:0,
+    opacity:1,
+    transition: {type:"spring",
+    bounce:0.4,
+    duration:1}
+  }
 
-// const Footer = styled.div`
-//     width : 100%;
-//     height : 300px;
-// `
+}
+
+const textAnimate1={
+    offscreen:{y:-100, opacity:0},
+    onscreen:{y:0,
+    opacity:1,
+    transition: {type:"spring",
+    bounce:0.4,
+    duration:1,
+    delay : 0.5}
+ }
+}
+ const textAnimate2={
+    offscreen:{y:-100, opacity:0},
+    onscreen:{y:0,
+    opacity:1,
+    transition: {type:"spring",
+    bounce:0.4,
+    duration:1,
+    delay : 0.9}
+ }
+
+}
 
 const AboutTeam = () => {
+
+
+
+
     return (
         <>
             <Container>
-                <h1>?</h1>
-                <div className="question">Corun은 어떤 팀인가요?</div>
-                <h1 className="quote-left">"</h1>
-                <h1 className="quote-right">"</h1>
+                <motion.div className="icon-bulb"
+                    variants={imageAnimate}
+                    initial={"offscreen"}
+                    whileInView={"onscreen"}
+                    viewport={{once:false, amount:0.5}}
+                    transition={{staggerChildren:0.5}}
+                >
+                    💡
+                </motion.div>
+                <motion.h1
+                    variants={textAnimate1}
+                    initial={"offscreen"}
+                    whileInView={"onscreen"}
+                >Corun은 어떤 팀일까요?</motion.h1>
+                <Link to ="/description">
+                    <motion.button
+                        variants={textAnimate2}
+                        initial={"offscreen"}
+                        whileInView={"onscreen"}
+                        whileHover={{ scale : 1.1 }}
+                        whileTap={{ scale : 1 }}
+                        transition={{ type: 'spring', stiffness: 120}}
+                    >Corun팀에 대해서 알아보기</motion.button>
+                </Link>
+
+
+
+                {/* <div className="background-element">
+                    <h1>?</h1>
+                    <div className="question">Corun은 어떤 팀인가요?</div>
+                    <h1 className="quote-left">"</h1>
+                    <h1 className="quote-right">"</h1>
+                </div>
                 <div className="content">
                     <div className="context1">저희는 부트캠프를 통해 배운 기술을 기반으로</div>
                     <div className="context2">다양한 프로젝트를 시도하는 프론트엔드 팀입니다.</div>
@@ -140,7 +136,7 @@ const AboutTeam = () => {
                         
                         <span className="icon-text">Corun의 프로젝트 보러가기</span>
                     </div>
-                </div>
+                </div> */}
             </Container>
             {/* <Footer>
 

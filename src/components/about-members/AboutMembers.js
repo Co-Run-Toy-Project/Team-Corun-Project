40279faa3f2ca8a36e419dc654bb091a7dbc8426
Oheun Kim <dhdms8696@naver.com>
2022-10-04 15:30:26 +0900

    TCP-43 스위치 시 정보창 구현 (#17)
    * TCP-24 깃허브, 블로그 연동
    
    * TCP-24 데이터 이름 수정
    
    * TCP-24 데이터 추가 및 수정
    
    * TCP-24 스위치 리팩토링
    
    * TCP-43 사진 추가
    
    * TCP-43 프로필 사진 props로 전달
    
    * TCP-43 props 받아 프로필 변경
    
    * TCP-43 데이터 수정
    
    * TCP-43 리팩토링
    
    * TCP-43 refactor: 코드 수정
    
    * TCP-43 refactor: 컴포넌트 분리
    
    - AboutMembers 최상위
    - InfoArea.js 각자 프로필
    - Switch 프로필 이미지 위치 변경
    
    * TCP-43 feat: 정보창 사라지게 하기
    
    * TCP-43 feat: 스위칭 애니메이션 구현
    
    - 감격스럽다
    
    * TCP-43 스위칭시 정보 변경
    
    * TCP-43 docs: 데이터 추가
    
    * TCP-43 style: 연동 버튼 정렬
    
    * TCP-43 docs: 역할 추가
    
    Co-authored-by: hun0613 <pshun7614@gmail.com>

--- src/components/about-members/AboutMembers.js
+++ src/components/about-members/AboutMembers.js
@@ -1,8 +1,9 @@
 // 한 번 한 페이지에 다 담아보겠습니다.
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import members from "../../data/members.json";
-import Switch from "./Switch";
+import memberList from "../../data/members.json";
+
+import InfoArea from "./InfoArea";
 
 const Container = styled.div`
   display: flex;
@@ -12,104 +13,25 @@ const Container = styled.div`
   padding: 50px;
 `;
 
-const ProfileContainer = styled.div`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-
-  width: 100%;
-  margin-left: 30px;
-  margin-bottom: 50px;
-`;
-
-const InfoCotainer = styled.div`
-  margin-left: 40px;
-`;
-
-const NameArea = styled.div`
-  display: flex;
-      
-
-  a {
-    text-decoration: none;
-    color: ${(props => props.theme.black)};
-
-    margin-left: 10px;
-
-    text-align: center;
-    align-self: center;
-
-    width: 100px;
-    height: 36px;
-    
-
-    border: none;
-    border-radius: 25px;
-    background-color: ${(props) => props.theme.gray};
-
-    &:hover {
-      transition: 0.5s;
-      background-color: ${(props) => props.theme.bgColor};
-    }
-
-    &:visited {
-      background-color: ${(props) => props.theme.blue};
-    }
-  }
-`;
-
-const SocialBtn = styled.a`
-  margin-left: 10px;
-  width: 100px;
-  height: 36px;
-  opacity: 80%;
-
-  border: none;
-  border-radius: 15px;
-  z-index: -1;
-
-  &:hover {
-    transition: 0.5s;
-    background-color: ${(props) => props.theme.gray};
-  }
-`;
-
-const Description = styled.div`
-  /* 개행 문자(\n) 인식 */
-  white-space: pre-wrap;
-`;
-
-export default function AboutMembers() {
-  const IterationSample = () => {
-    // 초기상태 설정
-    const [members, setMembers] = useState(members);
-  };
-
-  const Name = () =>
-    members.map((member) => (
-      <ProfileContainer key={member.id}>
-        <Switch />
-        <InfoCotainer>
-          <NameArea>
-            <h1>{member.name}</h1>
-            {/* ㅡㅡ 걍 a태그면 되는 거였네 괜히 땅팠네 */}
-            <a target="_blank" href={member.github}>
-              Github
-            </a>
-            <a target="_blank" href={member.blog}>
-              Blog
-            </a>
-          </NameArea>
-          <h2>{member.role}</h2>
-          <Description>{member.strength}</Description>
-        </InfoCotainer>
-      </ProfileContainer>
-    ));
+const AboutMembers = () => {
+  let [members, setMembers] = useState(memberList);
 
   return (
     <Container>
       <h1>CoRun의 구성원을 소개합니다</h1>
-      <Name />
+      {members.map((member) => (
+        <InfoArea 
+        key = {member.id}
+        name = {member.name}
+        profile={member.profile}
+        strength={member.strength}
+        role={member.role}
+        github={member.github}
+        blog={member.blog}/>
+      ))}
+      ;
     </Container>
   );
-}
+};
+
+export default AboutMembers;
--- src/components/about-members/Switch.js
+++ src/components/about-members/Switch.js
@@ -1,39 +1,31 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
-// button 말고 input 태그로 수정
-const ChangeCircle = styled.button`
-    position: relative;
+// button 말고 img 태그로 수정
+const ChangeCircle = styled.img`
+  position: relative;
   width: 300px;
   height: 300px;
   border: none;
   border-radius: 50%;
-
-  background-image: url("image/dummyProfile.jpg");
   background-size: 300px;
-  /* background-color: ${(props) => props.theme.gray}; */
+ 
   left: 0%;
   transition: all 0.6s ease-in-out;
 
-  /* 아 & 안 써서 이동 안했네 ㅅ.....* */
   &.open {
     background-image: url("image/dummyTech.jpg");
     background-size: 350px;
-    content: '';
+    content: "";
     left: 70%;
   }
 `;
 
-const Switch = () => {
-  // 초기상태
-  const [state, setState] = useState(false);
-
-  const toggle = () => {
-    // toggle 누르면 state 바뀜
-    setState(!state);
-  };
-
+// 프로필 창이 변경되는 함수
+const Switch = (props) => {
   return (
+    // 여기서 a면 프로필 사진 b면 기술스택으로 할 것임
+    
      <ChangeCircle
       onClick={toggle}
       className={state ? "open" : ""}
